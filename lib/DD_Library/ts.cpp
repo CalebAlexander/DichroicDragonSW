3a912b40ea9996e75deb7abb89aa9ec8dffa0e8a
Caleb Alexander <calebsalexander@gmail.com>
2023-03-30 17:21:16 -0400

    Added support for local task storage pointer

--- lib/DD_Library/ts.cpp
+++ lib/DD_Library/ts.cpp
@@ -12,7 +12,7 @@
 #define _TASK_SLEEP_ON_IDLE_RUN // Enable 1 ms SLEEP_IDLE powerdowns between tasks if no callback methods were invoked during the pass
 //  #define _TASK_STATUS_REQUEST    // Compile with support for StatusRequest functionality - triggering tasks on status change events in addition to time only
 //  #define _TASK_WDT_IDS           // Compile with support for wdt control points and task ids
-//  #define _TASK_LTS_POINTER       // Compile with support for local task storage pointer
+#define _TASK_LTS_POINTER // Compile with support for local task storage pointer
 //  #define _TASK_PRIORITY          // Support for layered scheduling priority
 //  #define _TASK_MICRO_RES         // Support for microsecond resolution
 //  #define _TASK_STD_FUNCTION      // Support for std::function (ESP8266 ONLY)
